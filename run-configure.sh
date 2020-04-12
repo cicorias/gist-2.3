5fb45ad344c7ab5804aba2dad94f1e66a6e4544b
Shawn Cicoria <github@cicoria.com>
2020-04-12 18:17:13 -0400

    push

--- run-configure.sh
+++ run-configure.sh
@@ -1,1 +1,1 @@
-CFLAGS="-Wno-unused-but-set-parameter -Wno-unused-but-set-variable -Wno-absolute-value" ./configure
+CFLAGS="-Wno-unused-but-set-parameter -Wno-unused-but-set-variable -Wno-absolute-value" ./configure --bindir=$HOME/bin/gist
